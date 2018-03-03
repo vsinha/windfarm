6ec41dc8a6efaf403ffd650c07b91e6896cb0906
Viraj Sinha <virajosinha@gmail.com>
2018-03-03 09:52:18 -0800

    Add project files.

---
+++ windfarm/windfarm.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		windfarm.ino
+ Created:	3/3/2018 9:52:14 AM
+ Author:	viraj
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
